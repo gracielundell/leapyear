16dcbadbb6a8e0778117b9bb1ef6f14e3f0693f0
blake scott and gracie lundell <blakerscott@gmail.com>
2016-01-19 10:38:54 -0800

    Add test to check if input is a number

--- spec/specs.js
+++ spec/specs.js
@@ -14,4 +14,9 @@ describe('leapYear', function() {
   it("is true for years divisible by 400", function() {
     expect(leapYear(2000)).to.equal(true);
   });
+
+  it("is not a number", function() {
+    expect(leapYear(NaN)).to.equal(false);
+  });
+
 });
